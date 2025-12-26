2ad929f317a497ce9cccf37f923f5126f24bf0d8
hung tran <tranmanh.hungdn1201@gmail.com>
2025-12-26 13:48:02 +0700

    update route

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { LanguageProvider } from "./contexts/LanguageContext";
 import { EventInfoPage } from "./components/EventInfoPage";
 import { RegistrationForm } from "./components/RegistrationForm";
@@ -7,12 +7,12 @@ import { RegistrationForm } from "./components/RegistrationForm";
 export function AppRouter() {
   return (
     <LanguageProvider>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<EventInfoPage />} />
           <Route path="/register" element={<RegistrationForm />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </LanguageProvider>
   );
 }
